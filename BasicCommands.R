b63238e3a5bb1b12f63d8c028ca33ce34b6d7a24
profemagallanes-unmsm <jmagallanes@pucp.edu.pe>
2023-02-05 21:53:54 -0800

    update codes

--- BasicCommands.R
+++ BasicCommands.R
@@ -1,12 +1,15 @@
 
+# First steps
+
 # someSetup -------------
 # clean memory
 rm(list = ls())
 
-# github link to data:
-link="https://github.com/eScienceWinterSchool/PythonSession/raw/master/hdidem_plus.RDS"
-
 # collecting_DataTable -------------
+
+# github link to data:
+link="https://github.com/eScienceWinterSchool/PythonSession/raw/master/hdidemiso_plus.RDS"
+
 #opening data
 fileLink=url(link)
 mergedData=readRDS(fileLink)
@@ -17,45 +20,45 @@ names(mergedData)
 # data types-------------
 str(mergedData)
 
-# categorical exploration -----------
-## display frequency table of  Regime Types
-library(summarytools)
+# categorical exploration 
 
-freq(mergedData$Regimetype, 
-     plain.ascii = FALSE,
-     report.nas=FALSE)
+## frequency table (I)-----------
+(FTRegime=table(mergedData$Regimetype)) #counts
+
+## frequency table (II)-----------
+(FTRegimeShare=prop.table(table(mergedData$Regimetype))) #share
 
 # compute statistics -------------
+(mode=which.max(FTRegime))
 
-DescTools::Mode(mergedData$Regimetype)
-DescTools::Median(mergedData$Regimetype)
+(median=FTRegimeShare[cumsum(FTRegimeShare)>0.5][1])
 
 # prepare plot -------------
-barplot(table(mergedData$Regimetype),
+barplot(FTRegimeShare,
         main = "Democracy in the world (2021)",
         xlab = "Regime Type",
         ylab = "count of countries",
-        las=2,
         col='orange',
-        border='red',
-        cex.names=0.5)
+        border='red')
 
-# Explore Numeric 
+# Numeric exploration 
 
 ## table of summary statistics ----
-summary(mergedData[,c(9:13)]) # democracy indicators
+
+summary(Filter(is.numeric, mergedData)) # all numeric indicators
 
 # numBoxplot ------
-boxplot(mergedData[,c(9:13)],
+mM_Positions=grep('mM',names(mergedData))
+boxplot(mergedData[,mM_Positions],# all numeric indicators mM
         las=2,
         horizontal = T,
         cex.axis=0.3,
         col='lightblue',
         border='red')
 
-# linear regression -----
+# linear regression 
 
-## prepare regressions
+## Hipotheses and regressions -----
 h1=formula(HumanDevelopmentIndex~Functioningofgovernment)
 regre1=glm(h1,data=mergedData,family = "gaussian")
 
@@ -63,19 +66,23 @@ h2=formula(HumanDevelopmentIndex~Functioningofgovernment + Politicalparticipatio
 regre2=glm(h2,data=mergedData,family = "gaussian")
 
 # show result of first regression -----
-summary(regre1)
+summary(regre1) # results
 library(rsq)
-rsq(regre1)
+rsq(regre1) # rsquare
 
 # show result of second regression -----
 summary(regre2)
-lm.beta::lm.beta(regre2)
+lm.beta::lm.beta(regre2) # standardized coeff
 rsq(regre2,adj=T)
 
-# compare regression -----
-anova(regre1,regre2)
+# compare regressions -----
+anova(regre1,regre2,test = 'Chisq') 
 
-# logistic regression -----
+# logistic regression 
+
+## Hipotheses and regressions -----
+
+# data selection (just two values)
 Selection=c('Authoritarian','Flawed democracy')
 mergedData_Dico=mergedData[mergedData$Regimetype%in%Selection,]
 
@@ -91,5 +98,74 @@ summary(regreLogi1)
 # show result of second regression -----
 summary(regreLogi2)
 
-# compare regression -----
-anova(regreLogi1,regreLogi2)
+# compare regressions -----
+anova(regreLogi1,regreLogi2,test = 'LRT')
+
+## Clustering countries
+
+# subset ----
+positions_mM=grep("mM", names(mergedData)) #easy way detect positions
+dataToCluster= mergedData[,positions_mM]
+row.names(dataToCluster)=mergedData$Countryname
+
+# clustering with k-medoids -----
+set.seed(111) # for replicability
+# process
+library(cluster)
+distances=cluster::daisy(dataToCluster)
+res.pam=cluster::pam(distances,4,cluster.only = F)
+#saving result in subset
+dataToCluster$pam=res.pam$cluster
+
+# understanding cluster labels  ----
+aggregate(data=dataToCluster,Overallscore_mM~pam,mean)
+
+# recoding labels-----
+oldvalues = c(3,1,2,4)
+newvalues = ordered(c(1,2,3,4))  # Make this a factor
+recoded=newvalues[ match(dataToCluster$pam, oldvalues) ]
+
+# new column ----
+mergedData$pam=recoded
+
+## map file
+
+# open file -----
+# link in Github
+linkMap="https://github.com/eScienceWinterSchool/R_Session/raw/main/worldMap.geojson"
+# read in file
+library(sf)
+mapWorld=read_sf(linkMap)
+
+# columns ------
+names(mapWorld)
+
+# quick look ------
+head(mapWorld)
+
+# structure ------
+str(mapWorld)
+
+# merge ----
+mapWorld_Data=merge(mapWorld,mergedData,
+                    by.x = 'ISO3', 
+                    by.y = 'iso3')
+
+#plot ----
+library(RColorBrewer)
+TheColors <- brewer.pal(4, "Set1")
+plot(st_geometry(mapWorld),
+     col = 'grey90', 
+     main="Countries by HDI & Democracy", 
+     sub='(missing values in light grey)',border = NA)
+plot(mapWorld_Data['pam'], 
+     border='grey50',
+     lwd=0.4,
+     col=TheColors[mapWorld_Data$pam],
+     add=T) # line width 
+legend("bottomleft", 
+       fill=TheColors,
+       legend = newvalues, 
+     )
+
+
